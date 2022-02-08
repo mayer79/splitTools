b514b070b569ca6ac399cde99b193c575fcccc00
mayer79 <mayermichael79@gmail.com>
2022-02-08 07:25:22 +0100

    revised description file

--- packaging.R
+++ packaging.R
@@ -14,27 +14,24 @@ library(usethis)
 use_description(
   fields = list(
     Title = "Tools for Data Splitting",
-    Type = "Package",
     Version = "0.3.2",
-    Date = Sys.Date(),
     Description = "Fast, lightweight toolkit for data splitting. Data sets can be partitioned into disjoint groups (e.g. into training, validation, and test) or into (repeated) k-folds for subsequent cross-validation. Besides basic splits, the package supports stratified, grouped as well as blocked splitting. Furthermore, cross-validation folds for time series data can be created. See e.g. Hastie et al. (2001) <doi:10.1007/978-0-387-84858-7> for the basic background on data partitioning and cross-validation.",
     `Authors@R` = "person('Michael', 'Mayer', email = 'mayermichael79@gmail.com', role = c('aut', 'cre'))",
-    LazyData = NULL,
-    Maintainer = "Michael Mayer <mayermichael79@gmail.com>"
+    LazyData = NULL
   ),
   roxygen = TRUE
 )
 
+use_package("stats", "Imports")
+use_package("ranger", "Suggests")
+
 use_gpl_license(2)
+
 use_github_links() # use this if this project is on github
 
 # Your files that do not belong to the package itself (others are added by "use_* function")
 use_build_ignore(c("^packaging.R$", "[.]Rproj$", "^backlog$",
                    "^cran-comments.md$", "^logo.png$"), escape = FALSE)
-
-# Required external packages
-use_package("stats", "Imports")
-use_package("ranger", "Suggests")
 
 # If your code uses the pipe operator %>%
 # use_pipe()
